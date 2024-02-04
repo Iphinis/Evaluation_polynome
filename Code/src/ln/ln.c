aa003b593f8ce45e4d23a19efa4c98e89a365f5a
qgarot <quentingarot06@gmail.com>
2024-02-04 14:18:05 +0100

    Fix Tn(x)

--- Code/src/ln/ln.c
+++ Code/src/ln/ln.c
@@ -3,7 +3,7 @@
 double calcul_ln(double x) {
     double res = log(9);
     for (int k = 1; k <= 6; k++) {
-        res = res - 2 * pow(-1, k) / (k * pow(3, k));
+        res = res - 2 * pow(-1, k) / (k * pow(3, k)) * cos(k * acos(x));
     }
     return res;
 }
