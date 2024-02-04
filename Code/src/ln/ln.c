a47bc7f71f0c15f7b0aa53e78bbd33301dbd7e04
qgarot <quentingarot06@gmail.com>
2024-02-04 14:15:10 +0100

    Algo valeur approchée ln(10+6x)

---
+++ Code/src/ln/ln.c
@@ -0,0 +1,13 @@
+#include "ln.h"
+
+double calcul_ln(double x) {
+    double res = log(9);
+    for (int k = 1; k <= 6; k++) {
+        res = res - 2 * pow(-1, k) / (k * pow(3, k));
+    }
+    return res;
+}
+
+int verif_prec(double val_th, double val_empirique) {
+    return abs(val_th - val_empirique) < 1e-4;
+}
---
+++ Code/src/ln/ln.h
@@ -0,0 +1,22 @@
+#ifndef LN_H
+#define LN_H
+
+#include <math.h>
+
+#define abs(x) (x >= 0 ? x : -x)
+
+/*
+* Algorithme de calcul approché de la valeur ln(10 + 6x) avec une erreur absolue strictement inférieure à 10^-4
+* Paramètre : -1 <= x <= 1
+* Valeur de retour : valeur approchée de ln(10 + 6x)
+*/
+double calcul_ln(double x);
+
+/*
+* Validation de la précision
+* Paramètres : 
+*   - val_th : valeur théorique
+*   - val_empirique : valeur approchée
+* Valeur de retour : 1 si la précision est suffisante (strictement inférieure à 10^-4), 0 sinon.
+*/
+int verif_prec(double val_th, double val_empirique);
